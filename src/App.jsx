4c9fb6d2cc8406b39c850716339383dd2c878748
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-05-04 14:19:19 -0600

    Proteccion de inicio de seccion

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,40 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/login';
+import RequireAuth from './components/RequireAuth';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />         {/* Login primero */}
-        <Route path="/" element={<Home />} />               {/* Página principal luego del login */}
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Login es público */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Las siguientes rutas están protegidas */}
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <RequireAuth>
+              <About />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
