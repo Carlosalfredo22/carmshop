1bae2ac72b49ca7fa542017fa10635dd99b32cae
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-05-25 06:32:45 -0600

    Carlos Alfredo
    RequireAuth administrador

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,13 +1,22 @@
-// src/components/RequireAuth.jsx
-import { Navigate } from 'react-router-dom';
+import { useLocation, Navigate } from "react-router-dom";
 
 function RequireAuth({ children }) {
-  const token = localStorage.getItem('token');
+  // Aquí verificamos si existe un token en localStorage para saber si está autenticado
+  const isAuthenticated = !!localStorage.getItem("token");
+  const isrole = !!localStorage.getItem("role");
+  const location = useLocation();
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
+  if (!isAuthenticated) {
+    // Si no está autenticado, redirige a login y guarda la ruta actual en state
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
+  if (isrole === "cliente") {
+      // Si es un cliente, redirige a la página de inicio del cliente
+      return <Navigate to="/cliente" state={{ from: location }} replace />;
+  }
+
+  // Si está autenticado, muestra el contenido protegido
   return children;
 }
 
